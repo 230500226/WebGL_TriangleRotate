3b31d30b72a6b24794df8ca2129ac37197d959d6
Shahied Rustin <97807830+snowyru@users.noreply.github.com>
2024-02-10 19:28:40 +0200

    itworks kinda

--- 3ShapesMove.js
+++ 3ShapesMove.js
@@ -7,7 +7,7 @@ function showError(errorText) {
     console.error(errorText); //console.log(errorText) for redundant error message
 }
 
-function testFunction(){
+function mainFunction(){
     const canvas = document.getElementById("IDcanvas");
     if (!canvas){
         showError("Can't find canvas reference");
@@ -24,8 +24,9 @@ function testFunction(){
     const vertexShaderSourceCode = `#version 300 es
     precision mediump float;
     in vec2 vertexPosition;
+    uniform vec2 uPosition;
     void main() {
-        gl_Position = vec4(vertexPosition, 0.0, 1.0);
+        gl_Position = vec4(vertexPosition + uPosition, 0.0, 1.0);
     }`;
 
     const vertexShader = gl.createShader(gl.VERTEX_SHADER);
@@ -180,36 +181,128 @@ function testFunction(){
     const starBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, starBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(starVertices), gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(position);
+
+    // Create direction and position logic
+    const canvasSizePixels = Math.min(canvas.width, canvas.height);
+
+    const squareSizeClipSpace = 1.6;
+    const squareSizePixels = squareSizeClipSpace * canvasSizePixels / 2;
+    var squarePositionLocation = gl.getUniformLocation(programSquare, 'uPosition');
+
+    const pentagonSizeClipSpace = 1.2;
+    const pentagonSizePixels = pentagonSizeClipSpace * canvasSizePixels / 2;
+    var pentagonPositionLocation = gl.getUniformLocation(programPentagon, 'uPosition');
+
+    const starSizeClipSpace = 0.178;
+    const starSizePixels = starSizeClipSpace * canvasSizePixels / 2;
+    var starPositionLocation = gl.getUniformLocation(programStar, 'uPosition');
+
+    const squareAnimDirection = {x:(Math.random() * 2 - 1) * 0.2, y:(Math.random() * 2 - 1) * 0.2};
+    const squareAnimPosition = {x:canvas.width / 2, y:canvas.height / 2};
+
+    const pentagonAnimDirection = {x:(Math.random() * 2 - 1) * 0.4, y:(Math.random() * 2 - 1) * 0.4};
+    const pentagonAnimPosition = {x:canvas.width / 2, y:canvas.height / 2};
+
+    const starAnimDirection = {x:(Math.random() * 2 - 1) * 0.6, y:(Math.random() * 2 - 1) * 0.6};
+    const starAnimPosition = {x:canvas.width / 2, y:canvas.height / 2};
 
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
-    gl.clearColor(0, 0, 0, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    gl.enableVertexAttribArray(position);
+    function draw() {
+        gl.clearColor(0, 0, 0, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    // Draw the square
-    gl.useProgram(programSquare);
-    gl.bindBuffer(gl.ARRAY_BUFFER, squareBuffer);
-    gl.vertexAttribPointer(position, 2, gl.FLOAT, false, 0, 0);
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
+        // Draw the square
+        gl.useProgram(programSquare);
 
-    // Draw the pentagon
-    gl.useProgram(programPentagon);
-    gl.bindBuffer(gl.ARRAY_BUFFER, pentagonBuffer);
-    gl.vertexAttribPointer(position, 2, gl.FLOAT, false, 0, 0);
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, 5);
+        const squareClipX = (squareAnimPosition.x / canvas.width) * 2 - 1;
+        const squareClipY = (squareAnimPosition.y / canvas.height) * -2 + 1;
 
-    // Draw the star
-    gl.useProgram(programStar);
-    gl.bindBuffer(gl.ARRAY_BUFFER, starBuffer);
-    gl.vertexAttribPointer(position, 2, gl.FLOAT, false, 0, 0);
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, 9);
+        gl.uniform2f(squarePositionLocation, squareClipX, squareClipY);
+        gl.bindBuffer(gl.ARRAY_BUFFER, squareBuffer);
+        gl.vertexAttribPointer(position, 2, gl.FLOAT, false, 0, 0);
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+        // Draw the pentagon
+        gl.useProgram(programPentagon);
+
+        const pentagonClipX = (pentagonAnimPosition.x / canvas.width) * 2 - 1;
+        const pentagonClipY = (pentagonAnimPosition.y / canvas.height) * -2 + 1;
+
+        gl.uniform2f(pentagonPositionLocation, pentagonClipX, pentagonClipY);
+        gl.bindBuffer(gl.ARRAY_BUFFER, pentagonBuffer);
+        gl.vertexAttribPointer(position, 2, gl.FLOAT, false, 0, 0);
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, 5);
+
+        // Draw the star
+        gl.useProgram(programStar);
+
+        const starClipX = (starAnimPosition.x / canvas.width) * 2 - 1;
+        const starClipY = (starAnimPosition.x / canvas.width) * - 2 + 1;
+
+        gl.uniform2f(starPositionLocation, starClipX, starClipY);
+        gl.bindBuffer(gl.ARRAY_BUFFER, starBuffer);
+        gl.vertexAttribPointer(position, 2, gl.FLOAT, false, 0, 0);
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, 9);
+    }
+
+    function update() {
+        squareAnimPosition.x += squareAnimDirection.x;
+        squareAnimPosition.y += squareAnimDirection.y;
+        if (squareAnimPosition.x - squareSizePixels/2 < 0 || squareAnimPosition.x + squareSizePixels/2 > canvas.width) {
+            squareAnimDirection.x *= -1;
+        }
+        if (squareAnimPosition.y - squareSizePixels/2 < 0 || squareAnimPosition.y + squareSizePixels/2 > canvas.height) {
+            squareAnimDirection.y *= -1;
+        }
+
+        pentagonAnimPosition.x += pentagonAnimDirection.x;
+        pentagonAnimPosition.y += pentagonAnimDirection.y;
+        if (pentagonAnimPosition.x - pentagonSizePixels/2 < 0 || pentagonAnimPosition.x + pentagonSizePixels/2 > canvas.width) {
+            pentagonAnimDirection.x *= -1;
+        }
+        if (pentagonAnimPosition.y - pentagonSizePixels/2 < 0 || pentagonAnimPosition.y + pentagonSizePixels/2 > canvas.height) {
+            pentagonAnimDirection.y *= -1;
+        }
+
+        starAnimPosition.x += starAnimDirection.x;
+        starAnimPosition.y += starAnimDirection.y;
+        if (starAnimPosition.x - starSizePixels/2 < 0 || starAnimPosition.x + starSizePixels/2 > canvas.width) {
+            starAnimDirection.x *= -1;
+        }
+        if (starAnimPosition.y - starSizePixels/2 < 0 || starAnimPosition.y + starSizePixels/2 > canvas.height) {
+            starAnimDirection.y *= -1;
+        }
+
+    }
+    var isAnimating = true;
+    //loop();
+    function toggleAnimation() {
+        isAnimating = !isAnimating;
+        if (isAnimating) {
+            loop();
+        }
+    }
+    function loop() {
+        if (!isAnimating) {
+            return;
+        }
+        update();
+        draw();
+        requestAnimationFrame(loop);
+    }
+
+    document.getElementById('btnStartStop').addEventListener('click', function(){
+        toggleAnimation();
+    });
+
 }
 
 try {
-    testFunction();
+    mainFunction();
 } catch (error) {
-    showError('failed to run testFunction() JS exception'+error);
+    showError('failed to run mainFunction() JS exception'+error);
 }
 
